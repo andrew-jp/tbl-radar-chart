c3ece2119bb16e62676d72822c2133a7aa6e2cb2
Andrew Pritchett <74275670+andrew-jp@users.noreply.github.com>
2025-11-18 13:42:38 -0700

    Update radar.js

--- radar.js
+++ radar.js
@@ -144,12 +144,24 @@ function drawRadarChart(container, labels, datasets, titleField) {
     },
     options: {
       responsive: true,
+      scales: {
+        r: {
+        min: 0,
+        max: 100,        // or 1
+        ticks: {
+          stepSize: 10,
+          callback: function (value) {
+            return value + '%';
+            }
+          }
+        }
+      },
       plugins: {
         legend: { position: 'top' },
         title: { display: false, text: `Radar Chart by ${titleField}` },
         tooltip: {
           // add custom tooltippery here
-        },
+        }
       },
 
       onClick: (event, elements) => {
